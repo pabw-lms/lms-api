9db381add69d25a0bf3af74d6f5fbf96ebbd46e9
Muhammad Bintang Ramadhan <muhbintangr19@gmail.com>
2023-05-14 20:47:37 +0700

    defining Api\V1\BookController

--- app/Http/Controllers/Api/V1/BookController.php
+++ app/Http/Controllers/Api/V1/BookController.php
@@ -6,62 +6,85 @@ use App\Models\Book;
 use App\Http\Requests\StoreBookRequest;
 use App\Http\Requests\UpdateBookRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return Book::all();
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
-    public function store(StoreBookRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'stock' => 'required',
+            'availability' => 'required',
+            'publisher' => 'required',
+            'pub_year' => 'required',
+            'pages' => 'required',
+        ]);
+
+        return Book::create($request->all());
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show(Book $book)
+    public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Book $book)
-    {
-        //
+        return Book::find($id);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(UpdateBookRequest $request, Book $book)
+    public function update(Request $request, $id)
     {
-        //
+        $book = Book::find($id);
+        $book->update($request->all());
+        return $book;
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(Book $book)
+    public function destroy($id)
     {
-        //
+        return Book::destroy($id);
+    }
+
+    /**
+     * Search book title.
+     *
+     * @param  string $title
+     * @return \Illuminate\Http\Response
+     */
+    public function search($title)
+    {
+        return Book::where('title', 'like', '%'.$title.'%')->get();
     }
 }
